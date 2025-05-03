677b0f956075f2d749fc8be5b46633cab53c8265
devomccar <devon.m.mccarthy@outlook.com>
2025-05-03 07:33:50 +0100

    Added os.exit to start server shell script

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -2,7 +2,9 @@
 
 set -eux # basically forcing the server to fail face, and print any commands to stdout to enable tracing
 
-set -eux
-
 # launch the JAR in the background
-nohup java -jar /home/ec2-user/app/*.jar &
+nohup java -jar /home/ec2-user/app/*.jar \
+   > app.log 2>&1 </dev/null &
+
+# immediately exit so CodeDeploy sees the script as done
+exit 0
