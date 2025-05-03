2d4514e94a1c82b8eca328b51da92b818d8e2d26
devomccar <devon.m.mccarthy@outlook.com>
2025-05-03 07:21:43 +0100

    Added fail fast lines to install/start server

--- scripts/install-java.sh
+++ scripts/install-java.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+set -eux # basically forcing the server to fail face, and print any commands to stdout to enable tracing
+
 # Install AWS's JDK
 yum install -y java-17-amazon-corretto-headless
--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+
+set -eux # basically forcing the server to fail face, and print any commands to stdout to enable tracing
+
+set -eux
+
 # launch the JAR in the background
 nohup java -jar /home/ec2-user/app/*.jar &
