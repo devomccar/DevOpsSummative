961830c6300ab2096568466deb7454b48701051c
devomccar <devon.m.mccarthy@outlook.com>
2025-05-03 07:36:22 +0100

    Added comments for clarity

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -2,9 +2,9 @@
 
 set -eux # basically forcing the server to fail face, and print any commands to stdout to enable tracing
 
-# launch the JAR in the background
+# launch the JAR
 nohup java -jar /home/ec2-user/app/*.jar \
-   > app.log 2>&1 </dev/null &
+   > app.log 2>&1 </dev/null & # in the background, and log to app.log
 
 # immediately exit so CodeDeploy sees the script as done
 exit 0
